3513ffe6f0fba63799557bf6bfd1d8a2eb22a7a4
christopherklint <christopher.klint@gmail.com>
2020-10-19 15:34:49 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,98 @@
+"""Blogly application."""
+
+from flask_debugtoolbar import DebugToolbarExtension
+from flask import Flask, render_template, redirect, request
+from models import db, connect_db, User
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+connect_db(app)
+db.create_all()
+
+app.config['SECRET_KEY'] = "SECRET!"
+debug = DebugToolbarExtension(app)
+
+
+@app.route("/")
+def redirect_to_users():
+    """Redirect to list of users."""
+
+    return redirect("/users")
+
+
+@app.route("/users")
+def list_users():
+    """List users on homepage."""
+
+    users = User.query.all()
+    return render_template("user_listing.html", users=users)
+
+
+@app.route("/users/new")
+def new_user():
+    """Show add form."""
+
+    return render_template("new_user_form.html")
+
+
+@app.route("/users/new", methods=["POST"])
+def post_new_user():
+    """Add user and redirect to list."""
+
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    image_url = request.form['image_url'] if request.form['image_url'] else None
+
+    user = User(first_name=first_name,
+                last_name=last_name, image_url=image_url)
+    db.session.add(user)
+    db.session.commit()
+
+    return redirect("/users")
+
+
+@app.route("/users/<int:user_id>")
+def show_user(user_id):
+    """Show info on a single user."""
+
+    user = User.query.get_or_404(user_id)
+    return render_template("user_details.html", user=user)
+
+
+@app.route("/users/<int:user_id>/edit")
+def edit_user(user_id):
+    """Edit info on a single user."""
+
+    user = User.query.get_or_404(user_id)
+    return render_template("edit_user_form.html", user=user)
+
+
+@app.route("/users/<int:user_id>/edit", methods=["POST"])
+def post_edit_user(user_id):
+    """Submit edited info on a single user."""
+
+    user = User.query.get_or_404(user_id)
+    user.first_name = request.form['first_name']
+    user.last_name = request.form['last_name']
+    user.image_url = request.form['image_url']
+
+    db.session.add(user)
+    db.session.commit()
+
+    return redirect("/users")
+
+
+@app.route("/users/<int:user_id>/delete", methods=["POST"])
+def delete_user(user_id):
+    """Delete user."""
+
+    user = User.query.get_or_404(user_id)
+
+    db.session.delete(user)
+    db.session.commit()
+
+    return redirect("/users")
---
+++ models.py
@@ -0,0 +1,34 @@
+"""Models for Blogly."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    db.init_app(app)
+
+
+class User(db.Model):
+    """User."""
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    first_name = db.Column(db.String(20),
+                           nullable=False)
+    last_name = db.Column(db.String(20),
+                          nullable=False)
+    image_url = db.Column(db.String(), nullable=True,
+                          default='https://images.unsplash.com/photo-1591102972305-213abaa76d6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=676&q=80')
+
+    def __repr__(self):
+        """Show info about user."""
+
+        u = self
+        return f"<User {u.id} {u.first_name} {u.last_name} {u.image_url}>"
